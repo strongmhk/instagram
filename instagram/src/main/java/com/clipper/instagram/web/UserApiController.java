2635223c180fdcd97e148cbdd43b25c939c9be87
strongmhk <kim6562166086@gmail.com>
2023-08-07 14:27:52 +0900

    feat: 유저 api 컨트롤러 생성, 회원 정보 수정 api 구현

---
+++ instagram/src/main/java/com/clipper/instagram/web/UserApiController.java
@@ -0,0 +1,53 @@
+package com.clipper.instagram.web;
+
+import com.clipper.instagram.config.auth.PrincipalDetails;
+import com.clipper.instagram.domain.user.User;
+import com.clipper.instagram.handler.ex.CustomValidationApiException;
+import com.clipper.instagram.service.UserService;
+import com.clipper.instagram.web.dto.CMRespDto;
+import com.clipper.instagram.web.dto.user.UserUpdateDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class UserApiController {
+
+    private final UserService userService;
+
+
+    @PutMapping("/user/{id}")
+    public CMRespDto<?> update(
+            @PathVariable int id,
+            @Valid @RequestBody UserUpdateDto userUpdateDto,
+            BindingResult bindingResult, // 꼭 @Valid가 적혀있는 다음 파라미터에 적어야됨
+            @AuthenticationPrincipal PrincipalDetails principalDetails) {
+
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+
+            throw new CustomValidationApiException("유효성검사 실패함", errorMap);
+        } else {
+            User userEntity = userService.updateInfo(id, userUpdateDto.toEntity());
+            principalDetails.setUser(userEntity); // 세션 정보 변경
+            return new CMRespDto<>(1, "회원수정완료", userEntity); // 응답시에 userEntity의 모든 getter 함수가 호출되고 JSON으로 파싱하여 응답한다.
+        }
+
+    }
+
+
+
+
+}
